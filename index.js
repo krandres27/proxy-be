80f1049a75ad88035daa0d66aa43a00ab6228b6e
Carlos Andres Gonzalez <cgonzalez@fullstacklabs.co>
2022-03-13 12:37:45 -0500

    env variable port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mainFetch = require('./api/fetch');
 
-const PORT = 8000;
+const PORT = process.env.PORT ?? 8000;
 
 const app = express();
 app.use(cors());
